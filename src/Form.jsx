a1d288600e718d34d1d89b7df3fdd6d21a100ac4
VIGNESH <107301285+Vikky-cse@users.noreply.github.com>
2023-09-27 14:47:39 +0530

    Update Form.jsx

--- src/Form.jsx
+++ src/Form.jsx
@@ -12,50 +12,98 @@ import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import axios from 'axios'; // Import axios for making API requests
+import { toast } from 'react-toastify'; // Import toast for showing notifications
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 const defaultTheme = createTheme();
 
-export default function SignInSide() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [gender, setGender] = useState(0);
-  const [hosteller, setHosteller] = useState(0);
-  const [password, setPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false); // State for password visibility
+export default function SignUp() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    gender: '',
+    isHosteller: false,
+    rollNo: '',
+    password: '',
+    confirmPassword: '',
+  });
 
-  const handleMailchange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handleNamechange = (event) => {
-    setName(event.target.value);
-  };
-
-  const handlePasswordchange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleGenderChange = (event) => {
-    setGender(event.target.value);
-  };
-
-  const handleHostellerChange = (event) => {
-    setHosteller(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+  const handleInputChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    setFormData({
+      ...formData,
+      [name]: type === 'checkbox' ? checked : value,
     });
   };
 
-  const toggleShowPassword = () => {
-    setShowPassword((prevShowPassword) => !prevShowPassword);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+ 
+    if (formData.name === '') {
+      toast.error('Please enter a valid User Name');
+      return;
+    }
+    if (!formData.email.includes('sece.ac.in')) {
+      toast.error('Provide a valid SECE Email Id');
+      return;
+    }
+    if (formData.gender === '') {
+      toast.error('Please select a gender');
+      return;
+    }
+    if (formData.rollNo === '') {
+      toast.error('Please select a valid Roll Number');
+      return;
+    }
+    if (formData.password === '') {
+      toast.error('Password cannot be empty');
+      return;
+    }
+    if (formData.password !== formData.confirmPassword) {
+      toast.error('Passwords do not match');
+      return;
+    }
+
+ 
+    const data = {
+      User_name: formData.email,
+      password: formData.password,
+      gender: formData.gender === 'male' ? 1 : 0,
+      isHosteller: formData.isHosteller ? 1 : 0,
+      rollNo: formData.rollNo,
+      name: formData.name,
+    };
+
+    try {
+     
+      const res = await axios.post('http://localhost:4000/auth/register', data);
+
+      if (res.status === 200) {
+        toast.error('User name already exists');
+      } else if (res.status === 202) {
+        toast.error('Roll Number already exists');
+      } else {
+        toast.success('User created successfully');
+        
+        setFormData({
+          name: '',
+          email: '',
+          gender: '',
+          isHosteller: false,
+          rollNo: '',
+          password: '',
+          confirmPassword: '',
+        });
+      }
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      toast.error('An error occurred while submitting the form');
+    }
   };
 
   return (
@@ -90,9 +138,10 @@ export default function SignInSide() {
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
-              Sign in
+              Sign up
             </Typography>
             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              
               <TextField
                 margin="normal"
                 required
@@ -101,7 +150,10 @@ export default function SignInSide() {
                 label="Name"
                 name="name"
                 autoFocus
+                value={formData.name}
+                onChange={handleInputChange}
               />
+              
               <TextField
                 margin="normal"
                 required
@@ -110,74 +162,99 @@ export default function SignInSide() {
                 label="Email Address"
                 name="email"
                 autoComplete="email"
-                autoFocus
+                value={formData.email}
+                onChange={handleInputChange}
               />
+              
               <FormControl fullWidth margin="normal" required>
                 <InputLabel>Gender</InputLabel>
                 <Select
                   label="Gender"
-                  value={gender}
-                  onChange={handleGenderChange}
-                  inputProps={{
-                    name: 'gender',
-                    id: 'gender',
-                  }}
+                  name="gender"
+                  value={formData.gender}
+                  onChange={handleInputChange}
                 >
                   <MenuItem value="male">Male</MenuItem>
                   <MenuItem value="female">Female</MenuItem>
-                  <MenuItem value="other">Other</MenuItem>
                 </Select>
               </FormControl>
-              <FormControl fullWidth margin="normal" required>
+              
+              <FormControl fullWidth margin="normal">
                 <InputLabel>Hosteller</InputLabel>
                 <Select
                   label="Hosteller"
-                  value={hosteller}
-                  onChange={handleHostellerChange}
-                  inputProps={{
-                    name: 'hosteller',
-                    id: 'hosteller',
-                  }}
+                  name="isHosteller"
+                  value={formData.isHosteller}
+                  onChange={handleInputChange}
                 >
-                  <MenuItem value="yes">Yes</MenuItem>
-                  <MenuItem value="no">No</MenuItem>
+                  <MenuItem value={true}>Yes</MenuItem>
+                  <MenuItem value={false}>No</MenuItem>
                 </Select>
               </FormControl>
+             
               <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type={showPassword ? 'text' : 'password'} // Toggle between text and password type
-              id="password"
-              autoComplete="current-password"
-              InputProps={{
-                endAdornment: (
-                  <Button onClick={toggleShowPassword} variant="text" size="small">
-                    {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
-                  </Button>
-                ),
-              }}
-            />
-              <Button
-                onClick={togglePasswordVisibility} // Toggle password visibility on button click
-                
-                variant="outlined"
-                sx={{ mt: 2 ,width:"80%"}}
-              >
-                {showPassword ? 'Hide Password' : 'Show Password'}
-              </Button>
+                margin="normal"
+                required
+                fullWidth
+                id="rollNo"
+                label="Roll Number"
+                name="rollNo"
+                value={formData.rollNo}
+                onChange={handleInputChange}
+              />
+              
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
+             
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="confirmPassword"
+                label="Confirm Password"
+                type="password"
+                id="confirmPassword"
+                autoComplete="current-password"
+                value={formData.confirmPassword}
+                onChange={handleInputChange}
+              />
+             
               <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
+                control={
+                  <Checkbox
+                    name="showPassword"
+                    checked={formData.showPassword}
+                    onChange={handleInputChange}
+                    color="primary"
+                  />
+                }
+                label="Show Password"
+              />
+              
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    name="remember"
+                    checked={formData.remember}
+                    onChange={handleInputChange}
+                    color="primary"
+                  />
+                }
                 label="Remember me"
               />
               <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-                Sign In
+                Sign Up
               </Button>
-              <Grid container>
-                
-              </Grid>
             </Box>
           </Box>
         </Grid>
